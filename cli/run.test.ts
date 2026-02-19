63ebd1ad1de77c3d99ca2c6e217b753a53b3ec8b
WhyK <windchimeyk@gmail.com>
2026-02-20 02:53:35 +0900

    docs: add ignore comment

--- cli/run.test.ts
+++ cli/run.test.ts
@@ -2,7 +2,8 @@ import { afterAll, describe, it } from "@std/testing/bdd";
 import { expect } from "@std/expect";
 import { join } from "@std/path";
 import { exists } from "@std/fs";
-import { configure } from "jsr:@zip-js/zip-js";
+// deno-lint-ignore no-import-prefix -- Web Worker使用時にタイマーによるリークが発生するため、直インポートを許容
+import { configure } from "jsr:@zip-js/zip-js@^2.8.17";
 import { CliError } from "./error.ts";
 import { DataPropertyError, FileTypeError } from "@tsuduri/core";
 import { run } from "./run.ts";
